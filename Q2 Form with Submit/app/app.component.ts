525cfcae1210bd16a2af9075b05c8c7eadda35a5
yogesh-shinde <40517239+yogesh-shinde@users.noreply.github.com>
2018-07-13 15:44:26 +0530

    Assi-5(Forms)

---
+++ Q2 Form with Submit/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+
+  public AcceptData(valu1,value2,value3,value4,value5,value6)
+  {
+    console.log(valu1,value2,value3,value4,value5,value6);
+  }
+}
